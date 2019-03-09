fcdd9ded58400efefcd78774f392e28ea9797f1b
Michael561030 <maxmiha221@gmail.com>
2019-03-09 18:47:31 +0200

    optimize code

--- index.js
+++ index.js
@@ -1,95 +1,74 @@
-let count = 0;
-let allblocks = document.getElementsByClassName('block');
-let whoIsMove = document.getElementById('whoismove')
+let count = 0;// counter
+const allBlocks = document.getElementsByClassName('block'); //all blocks on a field
+const whoIsMove = document.getElementById('whoismove'); // info field
+const game = document.getElementById('game');// main field
 
 // building field within 9 squares
 function buildField() {
     for (let index =0; index<9; index++){
-        document.getElementById('game').innerHTML += '<div class="block"></div>'
-        document.getElementById('whoismove').innerHTML='X move first'
+        game.innerHTML += '<div class="block"></div>';
+        whoIsMove.innerHTML='X move first';
     }
 }
-
 //draw X or O
 function drawTicTacToe() {
-    buildField()
-    document.getElementById('game').onclick = function (event) {
-        //checking if item has className 'block' and empty, draw X or O
-        if ((event.target.className == 'block') && (event.target.textContent == '')) {
-           if (count % 2 == 0) {
-               event.target.innerHTML = 'x';
-               whoIsMove.innerHTML='O move next';
-           }
-           else {
-               event.target.innerHTML = 'o';
-               whoIsMove.innerHTML = 'X move next';
-           }
-
-        count++;
-
-        }
-
-        checkWin()
-        reset()
-        isDraw()
+    buildField();
+    for (let index=0; index < allBlocks.length; index++)
+        allBlocks[index].onclick = function (event) {
+            //checking if item has className 'block' and empty, draw X or O
+            if ((event.target.className == 'block') && (event.target.textContent == '')) {
+               if (count % 2 == 0 ) {
+                   event.target.innerHTML = 'x';
+                   whoIsMove.innerHTML='O move next';
+               }
+               else {
+                   event.target.innerHTML = 'o';
+                   whoIsMove.innerHTML = 'X move next';
+               }
+            count++;
+            }
+            checkWin(allBlocks);
+            writeWinner()
+            isDraw();
     }
+    reset();
 }
 // reset all fields
 function reset(){
     document.getElementById('reset').onclick=function () {
-        for (let i=0; i<allblocks.length; i++){
-            allblocks[i].innerHTML='';
+        for (let i=0; i<allBlocks.length; i++){
+            allBlocks[i].innerHTML='';
         }
-        document.getElementById('whoismove').innerHTML='X move first'
+        whoIsMove.innerHTML='X move first'
         count=0;
     }
 }
 // check all combinations for win
-function checkWin() {
-
-
-    let a = allblocks;
-    if (a[0].textContent == 'x' && a[1].textContent == 'x' && a[2].textContent == 'x') return whoIsMove.innerHTML = 'X won'
-    if (a[3].textContent == 'x' && a[4].textContent == 'x' && a[5].textContent == 'x') return whoIsMove.innerHTML = 'X won';
-    if (a[6].textContent == 'x' && a[7].textContent == 'x' && a[8].textContent == 'x') return whoIsMove.innerHTML = 'X won';
-    if (a[0].textContent == 'x' && a[3].textContent == 'x' && a[6].textContent == 'x') return whoIsMove.innerHTML = 'X won';
-    if (a[1].textContent == 'x' && a[4].textContent == 'x' && a[7].textContent == 'x') return whoIsMove.innerHTML = 'X won';
-    if (a[2].textContent == 'x' && a[5].textContent == 'x' && a[8].textContent == 'x') return whoIsMove.innerHTML = 'X won';
-    if (a[0].textContent == 'x' && a[4].textContent == 'x' && a[8].textContent == 'x') return whoIsMove.innerHTML = 'X won';
-    if (a[2].textContent == 'x' && a[4].textContent == 'x' && a[6].textContent == 'x') return whoIsMove.innerHTML = 'X won';
-
-    if (a[0].textContent == 'o' && a[1].textContent == 'o' && a[2].textContent == 'o') return whoIsMove.innerHTML = 'O won';
-    if (a[3].textContent == 'o' && a[4].textContent == 'o' && a[5].textContent == 'o') return whoIsMove.innerHTML = 'O won';
-    if (a[6].textContent == 'o' && a[7].textContent == 'o' && a[8].textContent == 'o') return whoIsMove.innerHTML = 'O won';
-    if (a[0].textContent == 'o' && a[3].textContent == 'o' && a[6].textContent == 'o') return whoIsMove.innerHTML = 'O won';
-    if (a[1].textContent == 'o' && a[4].textContent == 'o' && a[7].textContent == 'o') return whoIsMove.innerHTML = 'O won';
-    if (a[2].textContent == 'o' && a[5].textContent == 'o' && a[8].textContent == 'o') return whoIsMove.innerHTML = 'O won';
-    if (a[0].textContent == 'o' && a[4].textContent == 'o' && a[8].textContent == 'o') return whoIsMove.innerHTML = 'O won';
-    if (a[2].textContent == 'o' && a[4].textContent == 'o' && a[6].textContent == 'o') return whoIsMove.innerHTML = 'O won';
+function checkWin(a) {
+    if (a[0].textContent === a[1].textContent && a[1].textContent === a[2].textContent) return a[0].textContent;
+    if (a[3].textContent === a[4].textContent && a[4].textContent === a[5].textContent) return a[3].textContent;
+    if (a[6].textContent === a[7].textContent && a[7].textContent === a[8].textContent) return a[6].textContent;
+    if (a[0].textContent === a[3].textContent && a[3].textContent === a[6].textContent) return a[0].textContent;
+    if (a[1].textContent === a[4].textContent && a[4].textContent === a[7].textContent) return a[1].textContent;
+    if (a[2].textContent === a[5].textContent && a[5].textContent === a[8].textContent) return a[2].textContent;
+    if (a[0].textContent === a[4].textContent && a[4].textContent === a[8].textContent) return a[0].textContent;
+    if (a[2].textContent === a[4].textContent && a[4].textContent === a[6].textContent) return a[2].textContent;
     else return null
-
-
 }
 //check all conditions for draw
 function  isDraw() {
-    let countX = 0;
-    let countO = 0;
-    for (let i=0; i<allblocks.length; i++){
-        if (allblocks[i].textContent == 'x'){
-            countX++
-        }
-        if (allblocks[i].textContent == 'o'){
-            countO++
-        }
-        if ((countX==5) & (countO==4) & (checkWin() != null)) whoIsMove.innerHTML='Draw'
-
-
-
+        if (count>8 && checkWin(allBlocks)==null) whoIsMove.innerHTML='Draw';
+}
+//write who is winner in info field
+function writeWinner() {
+    if (checkWin(allBlocks)=='x' || checkWin(allBlocks)=='o'){
+        whoIsMove.innerHTML= checkWin(allBlocks).toUpperCase()+' won';
     }
+    else return null;
+
 }
 
-drawTicTacToe()
-
+drawTicTacToe();
 
 // let arrWinCase=[
 //     [0,1,2],
