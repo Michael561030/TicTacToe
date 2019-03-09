d4d3cf202b0ef8fe8da9c5312320eccb24bc4268
Michael561030 <maxmiha221@gmail.com>
2019-03-09 17:23:00 +0200

    add function "Draw1"

--- index.js
+++ index.js
@@ -49,36 +49,24 @@ function checkWin() {
 
 
     let a = allblocks;
+    if (a[0].textContent == 'x' && a[1].textContent == 'x' && a[2].textContent == 'x') return whoIsMove.innerHTML = 'X won'
+    if (a[3].textContent == 'x' && a[4].textContent == 'x' && a[5].textContent == 'x') return whoIsMove.innerHTML = 'X won';
+    if (a[6].textContent == 'x' && a[7].textContent == 'x' && a[8].textContent == 'x') return whoIsMove.innerHTML = 'X won';
+    if (a[0].textContent == 'x' && a[3].textContent == 'x' && a[6].textContent == 'x') return whoIsMove.innerHTML = 'X won';
+    if (a[1].textContent == 'x' && a[4].textContent == 'x' && a[7].textContent == 'x') return whoIsMove.innerHTML = 'X won';
+    if (a[2].textContent == 'x' && a[5].textContent == 'x' && a[8].textContent == 'x') return whoIsMove.innerHTML = 'X won';
+    if (a[0].textContent == 'x' && a[4].textContent == 'x' && a[8].textContent == 'x') return whoIsMove.innerHTML = 'X won';
+    if (a[2].textContent == 'x' && a[4].textContent == 'x' && a[6].textContent == 'x') return whoIsMove.innerHTML = 'X won';
 
-    if (a[0].textContent == 'x' && a[1].textContent == 'x' && a[2].textContent == 'x') {
-        whoIsMove.innerHTML = 'X won';
-        var draw = SVG('drawing').size(300, 130)
-        var line = draw.line(0, 100, 100, 0).move(20, 20)
-        line.stroke({ color: '#f06', width: 10, linecap: 'round' })
-        // var context = document.getElementById('canvas');
-        // var ctx = canvas.getContext("2d");
-        // ctx.moveTo(0,-200);
-        // ctx.lineTo(300,50);
-        // ctx.lineWidth='5';
-        // ctx.stroke();
-    }
-    if (a[3].textContent == 'x' && a[4].textContent == 'x' && a[5].textContent == 'x') whoIsMove.innerHTML = 'X won';
-    if (a[6].textContent == 'x' && a[7].textContent == 'x' && a[8].textContent == 'x') whoIsMove.innerHTML = 'X won';
-    if (a[0].textContent == 'x' && a[3].textContent == 'x' && a[6].textContent == 'x') whoIsMove.innerHTML = 'X won';
-    if (a[1].textContent == 'x' && a[4].textContent == 'x' && a[7].textContent == 'x') whoIsMove.innerHTML = 'X won';
-    if (a[2].textContent == 'x' && a[5].textContent == 'x' && a[8].textContent == 'x') whoIsMove.innerHTML = 'X won';
-    if (a[0].textContent == 'x' && a[4].textContent == 'x' && a[8].textContent == 'x') whoIsMove.innerHTML = 'X won';
-    if (a[2].textContent == 'x' && a[4].textContent == 'x' && a[6].textContent == 'x') whoIsMove.innerHTML = 'X won';
-
-    if (a[0].textContent == 'o' && a[1].textContent == 'o' && a[2].textContent == 'o') whoIsMove.innerHTML = 'O won';
-    if (a[3].textContent == 'o' && a[4].textContent == 'o' && a[5].textContent == 'o') whoIsMove.innerHTML = 'O won';
-    if (a[6].textContent == 'o' && a[7].textContent == 'o' && a[8].textContent == 'o') whoIsMove.innerHTML = 'O won';
-    if (a[0].textContent == 'o' && a[3].textContent == 'o' && a[6].textContent == 'o') whoIsMove.innerHTML = 'O won';
-    if (a[1].textContent == 'o' && a[4].textContent == 'o' && a[7].textContent == 'o') whoIsMove.innerHTML = 'O won';
-    if (a[2].textContent == 'o' && a[5].textContent == 'o' && a[8].textContent == 'o') whoIsMove.innerHTML = 'O won';
-    if (a[0].textContent == 'o' && a[4].textContent == 'o' && a[8].textContent == 'o') whoIsMove.innerHTML = 'O won';
-    if (a[2].textContent == 'o' && a[4].textContent == 'o' && a[6].textContent == 'o') whoIsMove.innerHTML = 'O won';
-
+    if (a[0].textContent == 'o' && a[1].textContent == 'o' && a[2].textContent == 'o') return whoIsMove.innerHTML = 'O won';
+    if (a[3].textContent == 'o' && a[4].textContent == 'o' && a[5].textContent == 'o') return whoIsMove.innerHTML = 'O won';
+    if (a[6].textContent == 'o' && a[7].textContent == 'o' && a[8].textContent == 'o') return whoIsMove.innerHTML = 'O won';
+    if (a[0].textContent == 'o' && a[3].textContent == 'o' && a[6].textContent == 'o') return whoIsMove.innerHTML = 'O won';
+    if (a[1].textContent == 'o' && a[4].textContent == 'o' && a[7].textContent == 'o') return whoIsMove.innerHTML = 'O won';
+    if (a[2].textContent == 'o' && a[5].textContent == 'o' && a[8].textContent == 'o') return whoIsMove.innerHTML = 'O won';
+    if (a[0].textContent == 'o' && a[4].textContent == 'o' && a[8].textContent == 'o') return whoIsMove.innerHTML = 'O won';
+    if (a[2].textContent == 'o' && a[4].textContent == 'o' && a[6].textContent == 'o') return whoIsMove.innerHTML = 'O won';
+    else return null
 
 
 }
@@ -93,7 +81,7 @@ function  isDraw() {
         if (allblocks[i].textContent == 'o'){
             countO++
         }
-        if ((countX==5) & (countO==4)) whoIsMove.innerHTML='Draw'
+        if ((countX==5) & (countO==4) & (checkWin() != null)) whoIsMove.innerHTML='Draw'
 
 
 
