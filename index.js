c918a185165837a6d504d836e971c6119bc10f35
Michael561030 <maxmiha221@gmail.com>
2019-03-13 11:38:49 +0200

    fix code to draw 5x5 field

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const whoMoves = document.getElementById('whomoves'); // info field
 const game = document.getElementById('game');// main field
 const line = document.getElementsByClassName('line');
 const collectionOfWin = {
-    '0-2': 'firstHorizontalLine',
-    '3-5': 'secondHorizontalLine',
-    '6-8': 'thirdHorizontalLine',
-    '0-6': 'firstVerticalLine',
-    '1-7': 'secondVerticalLine',
-    '2-8': 'thirdVerticalLine',
-    '0-8': 'firstDiagonalLine',
-    '2-6': 'secondDiagonalLine'
+    0: 'firstHorizontalLine',
+    1: 'secondHorizontalLine',
+    2: 'thirdHorizontalLine',
+    3: 'fourthHorizontalLine',
+    4: 'fifthHorizontalLine',
+    5: 'firstVerticalLine',
+    6: 'secondVerticalLine',
+    7: 'thirdVerticalLine',
+    8: 'fourthVerticalLine',
+    9: 'fifthVerticalLine',
+    10: 'firstDiagonalLine',
+    11: 'secondDiagonalLine'
 };
 // building field within 9 squares
 function buildField() {
-    for (let index =0; index<9; index++){
+    for (let index =0; index<25; index++){
         game.innerHTML += '<div class="block"></div>';
     }
     whoMoves.innerHTML='X moves first';
@@ -37,70 +41,113 @@ function reset(){
         count=0;
     }
 }
-// check all combinations for win
-function checkWin(a) {
-    if ((a[0].textContent === a[1].textContent && a[1].textContent === a[2].textContent) && a[0].textContent !== '') {
-        return {content: a[0].textContent,
-            lineOfWinner:'0-2'};
-    }
-    if ((a[3].textContent === a[4].textContent && a[4].textContent === a[5].textContent) && a[3].textContent !== ''){
-        return {content: a[3].textContent,
-            lineOfWinner:'3-5'};
-    }
-    if ((a[6].textContent === a[7].textContent && a[7].textContent === a[8].textContent) && a[6].textContent !== '') {
-        return {content: a[6].textContent,
-            lineOfWinner:'6-8'};
-    }
-    if ((a[0].textContent === a[3].textContent && a[3].textContent === a[6].textContent)  && a[0].textContent !== '') {
-        return {content: a[0].textContent,
-            lineOfWinner:'0-6'};
-    }
-    if ((a[1].textContent === a[4].textContent && a[4].textContent === a[7].textContent)  && a[1].textContent !== '') {
-        return {content: a[1].textContent,
-            lineOfWinner:'1-7'};
-    }
-    if ((a[2].textContent === a[5].textContent && a[5].textContent === a[8].textContent)  && a[2].textContent !== '') {
-        return {content: a[2].textContent,
-            lineOfWinner:'2-8'};
-    }
-    if ((a[0].textContent === a[4].textContent && a[4].textContent === a[8].textContent)  && a[0].textContent !== ''){
-        return {content: a[0].textContent,
-            lineOfWinner:'0-8'};
-    }
-    if ((a[2].textContent === a[4].textContent && a[4].textContent === a[6].textContent)  && a[2].textContent !== '') {
-        return {content: a[2].textContent,
-            lineOfWinner:'2-6'};
-    }
-    else {
-        return {content: null,
-            lineOfWinner: null};
-    }
+function isX(n) {
+    return n === 'x';
 }
-//check all conditions for draw
-function  isDraw() {
-    if (count>8 && checkWin(allBlocks).content==null) whoMoves.innerHTML='Draw';
+function isO(n) {
+    return n === 'o';
 }
+//checking winner in vertical lines
+function checkWinVertical(firstIndex,lastIndex,array) {
+    let arrayOfValues=[];
+    while(lastIndex<=24){
+        let inArray=[];
+        for (let index=firstIndex; index<=lastIndex; index+=5){
+            inArray.push(array[index].innerHTML);
+        }
+        firstIndex++;
+        lastIndex++;
+        arrayOfValues.push(inArray);
+    }
+    return arrayOfValues
+}
+
+//checking winner in horizontal lines
+function checkWinHorizontal(firstIndex,lastIndex,array) {
+    let arrayOfValues=[];
+    while(firstIndex<=20){
+        let inArray=[];
+        for (let index=firstIndex; index<=lastIndex; index++){
+            inArray.push(array[index].innerHTML);
+
+        }
+    firstIndex+=5;
+    lastIndex+=5;
+    arrayOfValues.push(inArray);
+    }
+    return arrayOfValues;
+}
+
 //write who is winner in info field
-function writeWinner() {
-    if (checkWin(allBlocks).content === 'x' || checkWin(allBlocks).content === 'o'){
-        return whoMoves.innerHTML= checkWin(allBlocks).content.toUpperCase()+' has won';
+function writeWinner(array) {
+    let length = array.length;
+    try{
+        for (let i=0; i<=length; i++){
+            for (let j=0; j<array[i].length; j++ ){
+                if ((array[i].every(isX) && array[i].length===5)||((array[i].every(isO) && array[i].length===5))){
+                    return whoMoves.innerHTML = array[i][j].toUpperCase() + ' has won';
+                }
+            }
+        }
+        if (!array) return;
     }
-    if (!checkWin(allBlocks)) return;
+    catch(TypeError){}
+
+}
+
+//when nobody win
+function  isDraw() {
+    if (count>24) whoMoves.innerHTML='Draw';
+
 }
 //drawing win line on the field
 function drawWinLine(){
-    let winnerLine = checkWin(allBlocks).lineOfWinner;
-    for (key in collectionOfWin){
-        if( winnerLine === key)
-            document.getElementById(collectionOfWin[key]).style.display='block'
+    // draw horizontal lines
+    try {
+        let winnerLine = checkWinHorizontal(0,4,allBlocks);
+        let length =checkWinHorizontal(0,4,allBlocks).length ;
+        for (let i=0; i<=length; i++){
+            for (let j=0; j<winnerLine[i].length; j++ ){
+                if ((winnerLine[i].every(isX) && winnerLine[i].length===5)||((winnerLine[i].every(isO) && winnerLine[i].length===5))){
+                    for (let key in collectionOfWin){
+                            if( winnerLine.indexOf(winnerLine[i]) == key){
+                                document.getElementById(collectionOfWin[key]).style.display='block';
+                        }
+                    }
+
+                }
+            }
+        }
+
     }
+    catch(TypeError) {'something went wrong'}
+    //draw vertical lines
+    try {
+        let winnerLine = checkWinVertical(0,20,allBlocks);
+        let length =checkWinVertical(0,20,allBlocks).length ;
+        for (let i=0; i<=length; i++){
+            for (let j=0; j<winnerLine[i].length; j++ ){
+                if ((winnerLine[i].every(isX) && winnerLine[i].length===5)||((winnerLine[i].every(isO) && winnerLine[i].length===5))){
+                    for (let key in collectionOfWin){
+                        if( (winnerLine.indexOf(winnerLine[i])+5) == key){
+                            document.getElementById(collectionOfWin[key]).style.display='block';
+                        }
+                    }
+
+                }
+            }
+        }
+
+    }
+    catch(TypeError) {'something went wrong'}
+
 }
 //draw X or O
 function drawTicTacToe() {
     buildField();
     for (let index=0; index < allBlocks.length; index++){
         allBlocks[index].onclick = function (event) {
-            if (checkWin(allBlocks).content === 'x' || checkWin(allBlocks).content === 'o'){
+            if (whoMoves.innerHTML === 'X has won' || whoMoves.innerHTML === 'O has won'){
                 return;
             }
             //checking if item has className 'block' and empty, draw X or O
@@ -115,8 +162,11 @@ function drawTicTacToe() {
                 }
                 count++;
             }
-            checkWin(allBlocks);
-            writeWinner();
+            checkWinHorizontal(0,4,allBlocks);
+            checkWinVertical(0,20,allBlocks);
+            checkWinDiagonal(0,24,allBlocks);
+            writeWinner(checkWinHorizontal(0,4,allBlocks));
+            writeWinner(checkWinVertical(0,20,allBlocks));
             drawWinLine();
             isDraw();
         }
@@ -124,4 +174,7 @@ function drawTicTacToe() {
     reset();
 }
 drawTicTacToe();
+
+
+
 
